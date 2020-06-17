ec8964daff44313648df510fd32e4a8fb7f926ba
Dmitry Zharikov <zdimon77@gmail.com>
2020-06-17 18:32:56 +0300

    commit from zdimon77@gmail.com ----

--- backend/dj_prj/dj_prj/local.py
+++
@@ -1,9 +0,0 @@
-from .settings import BASE_DIR
-import os
-
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-
-BACKEND_URL = 'http://localhost:8787'
-
-
-TELEGRAMM_KEY = '729228816:AAGSjgoh0hfj-fwyMBo4UlGvGxHtnp6Z_Js'
